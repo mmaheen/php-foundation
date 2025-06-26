28036a53b17bf3000b16f9470b1e396714a292cb
mmaheen <mushrifmaheen@gmail.com>
2025-06-26 13:56:56 +0600

    Echo, <br>

--- 1st class/index.php
+++ 1st class/index.php
@@ -1,2 +1,10 @@
 <?php
-echo "Hello, World!";
+echo "Hello, World!\n";
+echo "Hello, World!<br>";
+echo "Hello";
+
+//Laravel Herd Software
+
+echo "<h1>This is a heading</h1>";
+
+//php -S localhost:8000 to run the server
